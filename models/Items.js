f45a6f2c6ec20f1c9c6c38d1c0401d952c75fb88
imurtaza97 <imurtaza97@gmail.com>
2025-01-22 12:47:20 +0530

    items model

--- models/Items.js
+++ models/Items.js
@@ -1,75 +1,82 @@
-const mongoose = require('mongoose');
-
 const itemSchema = new mongoose.Schema({
     businessId: { 
         type: mongoose.Schema.Types.ObjectId, 
         ref: 'Business', 
         required: true 
-    }, // Reference to the Business schema
-    warehouseId: { 
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: 'Warehouse', 
-    }, // Reference to the Warehouse schema
-    type: { 
-        type: String, 
-        enum: ['Services', 'Product'], 
-        required: true 
-    }, // Type of item (Services/Product)
-    itemName: { 
-        type: String, 
-        required: true 
-    }, // Name of the item
-    unit: { 
-        type: String, 
-        required: true 
-    }, // Unit of measurement for the item
-    description: { 
-        type: String 
-    }, // Item description
+    },
+    warehouseId: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Warehouse'
+        }
+    ], // Multiple warehouses support, each referenced by its ID
+    units: [
+        {
+            type: { 
+                type: String, 
+                enum: ['nos', 'size', 'length', 'weight'], 
+                required: true 
+            },
+            value: {
+                type: Number, 
+                required: true
+            },
+            unit: { 
+                type: String, 
+                required: true
+            },
+            description: { 
+                type: String 
+            }
+        }
+    ],
     sellInfo: { 
         price: { 
             type: mongoose.Schema.Types.Decimal128, 
             required: true 
-        }, // Price of the item
+        },
         currency: { 
             type: String, 
             required: true 
-        } // Currency used for the price
+        }
     },
     purchaseInfo: { 
         purchasePrice: { 
             type: mongoose.Schema.Types.Decimal128, 
             required: true 
-        }, // Purchase price of the item
+        }, 
         purchaseCurrency: { 
             type: String, 
             required: true 
-        }, // Currency used for the purchase price
+        }, 
         vendorId: { 
             type: mongoose.Schema.Types.ObjectId, 
             ref: 'Vendor'
-        }, // Reference to the Vendor schema
+        }, 
     },
     gst: { 
         intraStateGST: { 
-            type: mongoose.Schema.Types.Decimal128 
-        }, // Default GST for intra-state transactions
+            type: mongoose.Schema.Types.Decimal128,
+            default: 0 
+        },
         interStateGST: { 
-            type: mongoose.Schema.Types.Decimal128 
-        } // Default GST for inter-state transactions
+            type: mongoose.Schema.Types.Decimal128,
+            default: 0 
+        }
     },
     sku: { 
         type: String 
-    }, // Stock Keeping Unit identifier
+    },
     hsnOrSac: { 
         type: String 
-    }, // HSN/SAC code
+    },
     availableQuantity: { 
-        type: Number 
-    }, // Quantity available in stock
+        type: Number,
+        default: 0
+    },
     stockValue: { 
         type: mongoose.Schema.Types.Decimal128 
-    }, // Total value of stock
+    },
     createdAt: { 
         type: Date, 
         default: Date.now 
