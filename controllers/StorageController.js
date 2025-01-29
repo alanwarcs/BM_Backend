5252b745fb5ca0e27387c2f8ba08349ad1c23461
imurtaza97 <imurtaza97@gmail.com>
2025-01-29 10:25:20 +0530

    Storage model

--- controllers/StorageController.js
+++ controllers/StorageController.js
@@ -45,3 +45,42 @@ exports.addStorage = async (req, res) => {
         res.status(500).json({ success: false, message: 'Failed to add storage. Please try again later.' });
     }
 };
+
+// Get all storage records with pagination and filtering
+exports.getStorage = async (req, res) => {
+    const user = req.user;
+
+    if (!user || !user.businessId) {
+        return res.status(400).json({ success: false, message: 'Invalid user data.' });
+    }
+
+    const { page = 1, limit = 13, storageType, search } = req.query;
+    const query = { businessId: user.businessId };
+
+    if (storageType) {
+        query.storageType = storageType;
+    }
+
+    if (search) {
+        query.$or = [
+            { storageName: { $regex: search, $options: 'i' } },
+            { storageAddress: { $regex: search, $options: 'i' } },
+        ];
+    }
+
+    try {
+        const totalStorage = await Storage.countDocuments(query);
+        const storages = await Storage.find(query)
+            .skip((page - 1) * limit)
+            .limit(Number(limit));
+
+        res.status(200).json({
+            success: true,
+            message: 'Storage retrieved successfully.',
+            data: { storage: storages, pagination: { totalPages: Math.ceil(totalStorage / limit), currentPage: Number(page) } },
+        });
+    } catch (error) {
+        console.error('Error fetching storage:', error);
+        res.status(500).json({ success: false, message: 'Failed to retrieve storage. Please try again later.' });
+    }
+};
--- routes/storageRoutes.js
+++ routes/storageRoutes.js
@@ -1,10 +1,13 @@
 // routes/authRoutes.js
 
 const express = require('express');
-const { addStorage } = require('../controllers/StorageController');
+const { addStorage, getStorage } = require('../controllers/StorageController');
 const { authMiddleware } = require('../middlewares/authMiddleware'); // Assuming you have an auth middleware to protect routes
 
 const router = express.Router();
+
+// Route for get storages
+router.get('/', authMiddleware, getStorage);
 
 // Route for add new storage location
 router.post('/addstorage', authMiddleware, addStorage);
